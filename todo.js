ecc50c2f9eb6dd7313debb99ec0ddccfb4edf0bd
nirbhay7838 <nirbhaytiwari586@gmail.com>
2022-11-02 02:50:20 +0530

    yet to be done ...

---
+++ todo.js
@@ -0,0 +1,10 @@
+// 1. error handelling.
+// 2. corse error.
+// 3. firebase intigration.
+// 4. jwt intigration.
+// 5. aws deployment.
+// 6. azure deployment.
+// 7. security of node js aand testing writing test caases.
+// 8. scket implimetation
+// 9. multer implimentation
+// 10. node mailer implimentation
